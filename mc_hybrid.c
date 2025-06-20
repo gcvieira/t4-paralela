a9d1790e1ac20e373d738ff5a9d322e26ba0fc93
Guilherme Vieira <guilherme.camara@edu.pucrs.br>
2025-06-19 22:16:27 -0300

    nro de pontos por parametro e threads em variavel

--- mc_hybrid.c
+++ mc_hybrid.c
@@ -5,69 +5,77 @@
 #include <mpi.h>
 
 int main(int argc, char *argv[]) {
-    int total_pontos, pontos_por_proc;
-    int rank, size;
-    int local_dentro = 0, total_dentro = 0;
+	int total_pontos, pontos_por_proc;
+	int rank, size;
+	int local_dentro = 0, total_dentro = 0;
+	double inicio_total, fim_total;
 
-    double inicio_total, fim_total;
+	int num_threads = 4; // numero de threads
 
-    // Inicializa o MPI
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+	// Inicializa o MPI
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    // Processo 0 lê o número total de pontos
-    if (rank == 0) {
-        //printf("Informe o número total de pontos a gerar: ");
-        scanf("%d", &total_pontos);
-    }
+	// Processo 0 lê argumento da linha de comando
+	if (rank == 0) {
+		if (argc != 2) {
+			fprintf(stderr, "Uso: %s <total_de_pontos>\n", argv[0]);
+			MPI_Abort(MPI_COMM_WORLD, 1);
+		}
+		total_pontos = atoi(argv[1]);
+	}
 
-    // Broadcast do número total de pontos para todos os processos
-    MPI_Bcast(&total_pontos, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	// Broadcast do número total de pontos para todos os processos
+	MPI_Bcast(&total_pontos, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-    // Cada processo calcula sua parte
-    pontos_por_proc = total_pontos / size;
+	// Cada processo calcula sua parte
+	pontos_por_proc = total_pontos / size;
 
-    // Início da contagem de tempo (rank 0)
-    if (rank == 0)
-        inicio_total = MPI_Wtime();
+	// Define o número de threads OpenMP
+	omp_set_num_threads(num_threads);
 
-    // OpenMP: paraleliza dentro de cada processo
-    #pragma omp parallel
-    {
-        unsigned int seed = time(NULL) ^ (rank * 1000) ^ omp_get_thread_num();
-        int local = 0;
+	// Início da contagem de tempo (rank 0)
+	if (rank == 0) inicio_total = MPI_Wtime();
 
-        #pragma omp for
-        for (int i = 0; i < pontos_por_proc; i++) {
-            double x = (double)rand_r(&seed) / RAND_MAX;
-            double y = (double)rand_r(&seed) / RAND_MAX;
+	// OpenMP: paraleliza dentro de cada processo
+	#pragma omp parallel
+	{
+		unsigned int seed = time(NULL) ^ (rank * 1000) ^ omp_get_thread_num();
+		int local = 0;
 
-            if (x * x + y * y <= 1.0)
-                local++;
-        }
+		#pragma omp for
+		for (int i = 0; i < pontos_por_proc; i++) {
+			double x = (double)rand_r(&seed) / RAND_MAX;
+			double y = (double)rand_r(&seed) / RAND_MAX;
 
-        #pragma omp atomic
-        local_dentro += local;
-    }
+			if (x * x + y * y <= 1.0)
+				local++;
+		}
 
-    // Reduz todos os resultados locais em total_dentro no processo 0
-    MPI_Reduce(&local_dentro, &total_dentro, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+		#pragma omp atomic
+		local_dentro += local;
+	}
 
-    // Rank 0 calcula Pi e imprime resultado
-    if (rank == 0) {
-        double pi = 4.0 * total_dentro / total_pontos;
-        fim_total = MPI_Wtime();
-        double tempo_total = fim_total - inicio_total;
+	// Reduz todos os resultados locais em total_dentro no processo 0
+	MPI_Reduce(&local_dentro, &total_dentro, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
-        printf("\n--- RESULTADO HÍBRIDO (MPI + OpenMP) ---\n");
-        printf("Total de pontos:          %d\n", total_pontos);
-        printf("Total dentro do círculo:  %d\n", total_dentro);
-        printf("Estimativa de Pi:         %.10f\n", pi);
-        printf("Tempo total:              %.4f segundos\n", tempo_total);
-    }
+	// Rank 0 calcula Pi e imprime resultado
+	if (rank == 0) {
+		double pi = 4.0 * total_dentro / total_pontos;
+		fim_total = MPI_Wtime();
+		double tempo_total = fim_total - inicio_total;
 
-    // Finaliza MPI
-    MPI_Finalize();
-    return 0;
+		printf("\n--- RESULTADO HÍBRIDO (MPI + OpenMP) ---\n");
+		printf("Processos MPI:             %d\n", size);
+		printf("Threads OpenMP por processo: %d\n", num_threads);
+		printf("Total de pontos:           %d\n", total_pontos);
+		printf("Total dentro do círculo:   %d\n", total_dentro);
+		printf("Estimativa de Pi:          %.10f\n", pi);
+		printf("Tempo total:               %.4f segundos\n", tempo_total);
+	}
+
+	// Finaliza MPI
+	MPI_Finalize();
+	return 0;
 }
